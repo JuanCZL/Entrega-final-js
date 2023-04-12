d6e599ccfb8f676f06033028c2e4b2d0234f066e
Juan Carlos Zambrano <juzambrano12345@gmail.com>
2023-04-12 01:06:18 -0300

    de nuevo

---
+++ main.js
@@ -0,0 +1,11 @@
+const stockTortas = [
+    {id:1, nombre:"GalakCake", precio:5500, tamaño:"pequeña"},
+    {id:2, nombre:"GolfeadoCake", precio:5000, tamaño:"pequeña"},
+    {id:3, nombre:"MilhojasCake", precio:6500, tamaño:"pequeña"},
+    {id:4, nombre:"GalakCake2", precio:6300, tamaño:"mediana"},
+    {id:5, nombre:"GolfeadosCake2", precio:6000, tamaño:"mediana"},
+    {id:6, nombre:"MilhojasCake2", precio:7500, tamaño:"mediana"},
+    {id:7, nombre:"GalakCake3", precio:7300, tamaño:"grande"},
+    {id:8, nombre:"GolfeadosCake3", precio:7000, tamaño:"grande"},
+    {id:9, nombre:"MilhojasCake3", precio:8500, tamaño:"grande"},
+];
